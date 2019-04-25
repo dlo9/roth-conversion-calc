ea70ae0d48edc379585907ca6fc5347c45720f95
David <if_coding@fastmail.com>
2019-04-24 19:08:35 -0700

    2x improvement in short bench by removing chrono datetime calculations

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,6 @@ extern crate test;
 extern crate lazy_static;
 
 use pathfinding::prelude::astar;
-use chrono::Duration;
-use chrono::Datelike;
-use chrono::naive::NaiveDate;
 use failure::*;
 use std::convert::TryFrom;
 
@@ -19,10 +16,11 @@ pub struct ProjectArgs {
     roth_effective_annual_rate: f64,
     ira_present_value: u64,
     ira_effective_annual_rate: f64,
-    birthday: NaiveDate,
+    birth_year: u16,
+    birth_month: u8,
     // TODO: these dates should ALWAYS be Dec 31. 
-    end_date: NaiveDate,
-    now: NaiveDate,
+    start_year: u16,
+    end_year: u16,
 }
 
 impl ProjectArgs {
@@ -41,10 +39,13 @@ impl ProjectArgs {
             err_msg("IRA value must be >= 0")
         } else if self.ira_effective_annual_rate > 1.0 || self.ira_effective_annual_rate < 0.0 {
             err_msg("IRA rate must be between 0 and 1")
-        } else if self.birthday > self.now {
-            err_msg("Birthday must be <= now")
-        } else if self.end_date < self.now {
-            err_msg("End date must be >= now")
+        } else if self.birth_year > self.start_year {
+            err_msg("Birth year must be <= start year")
+        } else if self.start_year > self.end_year {
+            err_msg("End year must be >= start year")
+        // TODO: range.contains once stable: https://doc.rust-lang.org/std/ops/struct.Range.html#method.contains
+        } else if self.birth_month < 1 || self.birth_month > 12 {
+            err_msg("Birth month must be between 1 and 12")
         } else {
             return Ok(())
         })
@@ -56,7 +57,7 @@ impl ProjectArgs {
 pub struct State {
     adjusted_spendable_income: u64,
     pending_rollover: u64,
-    now: NaiveDate,
+    current_year: u16,
     roth_present_value: u64,
     ira_present_value: u64,
 }
@@ -71,7 +72,7 @@ struct Successors {
 impl Successors {
     pub fn new(parent: &State, args: &ProjectArgs) -> Successors {
         Successors {
-            time: parent.step_time(args).ok(),
+            time: parent.step_year(args).ok(),
             rollover: parent.step_rollover(1000),
         }
     }
@@ -96,10 +97,9 @@ impl Iterator for Successors {
 }
 
 impl State {
-    fn step_time(&self, args: &ProjectArgs) -> Result<(State, Cost), Error> {
-        let now = self.now.checked_add_signed(Duration::days(365)).ok_or_else(|| err_msg("overflow"))?;
+    fn step_year(&self, args: &ProjectArgs) -> Result<(State, Cost), Error> {
         // TODO: is the rollover & RMD meshing properly?
-        let ira_rmd = get_rmd(args.birthday, now, self.ira_present_value).checked_sub(self.pending_rollover).unwrap_or_default();
+        let ira_rmd = get_rmd(args.birth_year, args.birth_month, self.current_year, self.ira_present_value).checked_sub(self.pending_rollover).unwrap_or_default();
         let ira_value = ((self.ira_present_value as f64) * (1f64 + args.ira_effective_annual_rate - args.inflation_effective_annual_rate)) as u64;
         let ira_value = ira_value - self.pending_rollover - ira_rmd;
 
@@ -114,7 +114,7 @@ impl State {
             pending_rollover: 0,
             roth_present_value: roth_value,
             ira_present_value: ira_value,
-            now: now,
+            current_year: self.current_year + 1,
             // TODO: want to maximize income, not minimize tax
         }, tax)) 
     }
@@ -143,7 +143,7 @@ pub fn project(args: &ProjectArgs) -> Option<(Vec<State>, Cost)> {
         adjusted_spendable_income: 0,
         pending_rollover: 0,
         // TODO: Pass in from args instead, so tests are reproducible
-        now: args.now,
+        current_year: args.start_year,
         roth_present_value: args.roth_present_value,
         ira_present_value: args.ira_present_value,
     };
@@ -152,13 +152,13 @@ pub fn project(args: &ProjectArgs) -> Option<(Vec<State>, Cost)> {
                |ref s| Successors::new(s, args),
                // TODO: improve
                |ref s| get_tax(args.yearly_taxable_income_excluding_ira + s.pending_rollover),
-               |ref s| s.now >= args.end_date,
+               |ref s| s.current_year >= args.end_year,
                ))
 }
 
 // TODO: only applies if (spouse not sole beneficiary) || (their age >= your age - 10)
 // Worksheet: https://www.irs.gov/pub/irs-tege/uniform_rmd_wksht.pdf
-fn get_rmd_distribution_period(birthday: NaiveDate, current_year: i32) -> Option<f64> {
+fn get_rmd_distribution_period(birth_year: u16, birth_month: u8, current_year: u16) -> Option<f64> {
     lazy_static! {
         // Index 0 == age 70
         static ref DISTRIBUTION_PERIODS: [f64; 46] = [
@@ -169,17 +169,18 @@ fn get_rmd_distribution_period(birthday: NaiveDate, current_year: i32) -> Option
         ];
     }
 
-    let age_this_year = current_year - birthday.year();
+    let age_this_year = current_year - birth_year;
+    // TODO: try_from still necessary now that I'm using u16?
     Some(match usize::try_from(age_this_year).unwrap_or_default() {
-        x @ 70 if birthday.month() < 7 => DISTRIBUTION_PERIODS[x - 70],
+        x @ 70 if birth_month < 7 => DISTRIBUTION_PERIODS[x - 70],
         x @ 71 ... 115 => DISTRIBUTION_PERIODS[x - 70],
         x if x >= 115 => DISTRIBUTION_PERIODS[115 - 70],
         _ => return None,
     })
 }
 
-fn get_rmd(birthday: NaiveDate, now: NaiveDate, prior_year_ending_ira_value: u64) -> u64 {
-    if let Some(distribution_period) = get_rmd_distribution_period(birthday, now.year()) {
+fn get_rmd(birth_year: u16, birth_month: u8, year: u16, prior_year_ending_ira_value: u64) -> u64 {
+    if let Some(distribution_period) = get_rmd_distribution_period(birth_year, birth_month, year) {
         ((prior_year_ending_ira_value as f64) / distribution_period) as u64
     } else {
         0
@@ -220,47 +221,47 @@ mod tests {
 
     #[test]
     fn rmd_distribution_period_turns_70_june_30() {
-        assert_eq!(Some(27.4), get_rmd_distribution_period(NaiveDate::from_ymd(1949, 6, 30), 2019));
+        assert_eq!(Some(27.4), get_rmd_distribution_period(1949, 6, 2019));
     }
 
     #[test]
     fn rmd_distribution_period_turns_71_june_30() {
-        assert_eq!(Some(26.5), get_rmd_distribution_period(NaiveDate::from_ymd(1948, 6, 30), 2019));
+        assert_eq!(Some(26.5), get_rmd_distribution_period(1948, 6, 2019));
     }
 
     #[test]
     fn rmd_distribution_period_turns_70_july_1() {
-        assert_eq!(None, get_rmd_distribution_period(NaiveDate::from_ymd(1949, 7, 1), 2019));
+        assert_eq!(None, get_rmd_distribution_period(1949, 7, 2019));
     }
 
     #[test]
     fn rmd_distribution_period_turns_71_july_1() {
-        assert_eq!(Some(26.5), get_rmd_distribution_period(NaiveDate::from_ymd(1948, 7, 1), 2019));
+        assert_eq!(Some(26.5), get_rmd_distribution_period(1948, 7, 2019));
     }
 
     #[test]
     fn rmd_distribution_period_age_butween_70_and_115() {
-        assert_eq!(Some(11.4), get_rmd_distribution_period(NaiveDate::from_ymd(2019 - 90, 3, 5), 2019));
+        assert_eq!(Some(11.4), get_rmd_distribution_period(2019 - 90, 3, 2019));
     }
 
     #[test]
     fn rmd_distribution_period_age_115() {
-        assert_eq!(Some(1.9), get_rmd_distribution_period(NaiveDate::from_ymd(2019 - 115, 3, 5), 2019));
+        assert_eq!(Some(1.9), get_rmd_distribution_period(2019 - 115, 3, 2019));
     }
 
     #[test]
     fn rmd_distribution_period_age_greater_than_115() {
-        assert_eq!(Some(1.9), get_rmd_distribution_period(NaiveDate::from_ymd(2019 - 116, 3, 5), 2019));
+        assert_eq!(Some(1.9), get_rmd_distribution_period(2019 - 116, 3, 2019));
     }
 
     #[test]
     fn rmd_distribution_period_age_less_than_70() {
-        assert_eq!(None, get_rmd_distribution_period(NaiveDate::from_ymd(2019 - 69, 3, 5), 2019));
+        assert_eq!(None, get_rmd_distribution_period(2019 - 69, 3, 2019));
     }
 
     #[test]
     fn rmd_distribution_period_negative_age() {
-        assert_eq!(None, get_rmd_distribution_period(NaiveDate::from_ymd(2019 + 1, 3, 5), 2019));
+        assert_eq!(None, get_rmd_distribution_period(2019 + 1, 3, 2019));
     }
 
     #[test]
@@ -283,9 +284,10 @@ mod tests {
             roth_effective_annual_rate: 0.08,
             ira_present_value: 6000,
             ira_effective_annual_rate: 0.08,
-            birthday: NaiveDate::from_ymd(1955, 6, 3),
-            end_date: NaiveDate::from_ymd(2040, 12, 31),
-            now: NaiveDate::from_ymd(2019, 12, 31),
+            birth_year: 1955,
+            birth_month: 6,
+            start_year: 2019,
+            end_year: 2040,
         };
 
         b.iter(|| assert!(project(&args).is_some()));
@@ -300,10 +302,10 @@ mod tests {
             roth_effective_annual_rate: 0.08,
             ira_present_value: 6000,
             ira_effective_annual_rate: 0.08,
-            birthday: NaiveDate::from_ymd(1955, 6, 3),
-            end_date: NaiveDate::from_ymd(2040, 12, 31),
-            // O(2^n) scaling, n = end_year - now_year
-            now: NaiveDate::from_ymd(2035, 12, 31),
+            birth_year: 1955,
+            birth_month: 6,
+            start_year: 2035,
+            end_year: 2040,
         };
 
         b.iter(|| assert!(project(&args).is_some()));
