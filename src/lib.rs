77d46d63034238fb4f11e943c319f7245df7f778
David <if_coding@fastmail.com>
2019-04-30 23:50:45 -0700

    Compile to wasm

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,21 @@ extern crate test;
 #[macro_use]
 extern crate lazy_static;
 
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
 use failure::*;
 use num_traits::identities::Zero;
 use std::collections::vec_deque::VecDeque;
